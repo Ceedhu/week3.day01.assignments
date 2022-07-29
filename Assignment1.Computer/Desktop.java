beba3fe877b477cad6796c6d216801e02d9280d5
Sridhar <107329849+Ceedhu@users.noreply.github.com>
2022-07-29 11:40:12 +0530

    Add files via upload

---
+++ Assignment1.Computer/Desktop.java
@@ -0,0 +1,26 @@
+package org.system;
+
+
+
+public class Desktop  extends Computer{
+
+	
+		
+		
+	public void desktopdimensions()
+	{
+		System.out.println("Desktop size: 32 inches ");
+	}
+	
+	public static void main(String[] args) 
+	{
+		Desktop dk=new Desktop();
+		dk.desktopdimensions();
+		dk.ComputerModel();
+		
+		
+		// TODO Auto-generated method stub
+
+	}}
+
+
